3c0589c398d8b565dff2ec69f5818f235b48f4d0
EchoTheDeveloper <admin@echothedeveloper.com>
2025-04-05 10:55:47 +0100

    adding random stuff to make it push to trigger event

--- main.cpp
+++ main.cpp
@@ -1,3 +1,9 @@
 #include <iostream>
+#include <string>
 
-int main() { std::cout << "Hello, World!" << std::endl; }
+int main() {
+  std::cout << "Hello, World!" << std::endl;
+  std::string name;
+  std::cin >> name;
+  std::cout << name << std::endl;
+}
